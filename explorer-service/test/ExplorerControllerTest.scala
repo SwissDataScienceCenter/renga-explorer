4d3c70a7615993ef72f7999e63f433fe9cb15ca8
3C111 <digital_urn@hotmail.com>
2017-08-15 13:47:07 +0200

    Added external graph object for test purposes

--- explorer-service/test/ExplorerControllerTest.scala
+++ explorer-service/test/ExplorerControllerTest.scala
@@ -1,5 +1,19 @@
+
 import org.scalatest.FlatSpec
+
+
 
 class ExplorerControllerTest extends FlatSpec {
 
+  val jsonGraph = ImportJSONGraph
+  //val c = jsonGraph.g.V().valueMap().toList()
+
+
 }
+
+/* import scala.collection.JavaConverters._
+ val s1 = t.toStream.iterator().asScala
+ for (v <- s1) {
+   println(v)
+ }
+*/
