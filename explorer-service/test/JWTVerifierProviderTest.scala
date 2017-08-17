f2a42fd396617686cc935ea20006fa48e319d613
3C111 <digital_urn@hotmail.com>
2017-08-17 13:25:43 +0200

    Commit to move to new repo

--- explorer-service/test/JWTVerifierProviderTest.scala
+++ explorer-service/test/JWTVerifierProviderTest.scala
@@ -1,10 +1,7 @@
 package authorization
 
-import com.auth0.jwt.JWTVerifier
-import com.auth0.jwt.interfaces.DecodedJWT
 import org.scalatest.FlatSpec
 import org.scalatest.mockito.MockitoSugar
-import org.mockito.Mockito._
 
 class JWTVerifierProviderTest extends FlatSpec with MockitoSugar {
 
