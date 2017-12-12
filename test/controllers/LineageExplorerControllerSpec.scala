841bd05ff17203eb531686e167757acf3aacf23e
3C111 <digital_urn@hotmail.com>
2017-12-12 09:02:43 +0100

    # This is a combination of 9 commits. # The first commit's message is: Resolve merge issue
    # The 2nd commit message will be skipped:
    
    #	Stash for headers update
    
    # The 3rd commit message will be skipped:
    
    #	Fixed borked swagger
    
    # The 4th commit message will be skipped:
    
    #	Added test for lineage
    
    # The 5th commit message will be skipped:
    
    #	small improvement to test
    
    # The 6th commit message will be skipped:
    
    #	Negative test for lineage deployer controller
    
    # The 7th commit message will be skipped:
    
    #	Fixed issue with missing edgee
    
    # The 8th commit message will be skipped:
    
    #	Attempt at lineage from file
    
    # The 9th commit message will be skipped:
    
    #	fortify node metadata call

--- test/controllers/LineageExplorerControllerSpec.scala
+++ test/controllers/LineageExplorerControllerSpec.scala
@@ -18,7 +18,10 @@
 
 package controllers
 
+import java.util
+
 import authorization.{ JWTVerifierProvider, MockJWTVerifierProvider, MockTokenSignerProvider }
+import ch.datascience.graph.Constants
 import ch.datascience.graph.elements.persisted.PersistedVertex
 import ch.datascience.graph.elements.persisted.json._
 import ch.datascience.service.utils.persistence.graph.{ JanusGraphProvider, JanusGraphTraversalSourceProvider }
@@ -27,24 +30,21 @@ import ch.datascience.test.security.FakeRequestWithToken._
 import ch.datascience.test.utils.persistence.graph.MockJanusGraphProvider
 import ch.datascience.test.utils.persistence.scope.MockScope
 import com.auth0.jwt.JWT
-import helpers.ImportJSONStorageGraph
+import helpers.ImportJSONLineageGraph
 import org.scalatest.BeforeAndAfter
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.{ OneAppPerSuite, PlaySpec }
 import play.api.Application
 import play.api.inject.bind
 import play.api.inject.guice.GuiceApplicationBuilder
-import play.api.libs.json.Reads
+import play.api.libs.json.{ JsObject, Reads }
 import play.api.test.FakeRequest
+import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.__
 import play.api.test.Helpers._
 
 import scala.collection.JavaConverters._
 
-/**
- * Continued by 3C111 on 29.08.2017
- */
-
-class GenericExplorerControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSugar with BeforeAndAfter {
+class LineageExplorerControllerSpec extends PlaySpec with OneAppPerSuite with MockitoSugar with BeforeAndAfter {
 
   // Set the stage
   override def fakeApplication(): Application = new GuiceApplicationBuilder()
@@ -53,7 +53,7 @@ class GenericExplorerControllerSpec extends PlaySpec with OneAppPerSuite with Mo
     .overrides( bind[Scope].to[MockScope] )
     .build()
 
-  val genericController: GenericExplorerController = app.injector.instanceOf[GenericExplorerController]
+  val lineageController: LineageExplorerController = app.injector.instanceOf[LineageExplorerController]
 
   val graph = app.injector.instanceOf[JanusGraphProvider].get
   val g = app.injector.instanceOf[JanusGraphTraversalSourceProvider].get
@@ -66,49 +66,38 @@ class GenericExplorerControllerSpec extends PlaySpec with OneAppPerSuite with Mo
   implicit val reads: Reads[PersistedVertex] = PersistedVertexFormat
 
   before {
-    ImportJSONStorageGraph.populateGraph( graph )
+    ImportJSONLineageGraph.populateGraph( graph )
   }
 
   after {
     graph.traversal().V().drop().iterate()
   }
 
-  "The return graph subset controller" should {
-    "return a subset of nodes and edges from the graph" in {
-      val result = genericController.retrieveGraphSubset().apply( fakerequest )
-      val content = contentAsJson( result ).as[Seq[GraphSubSet]]
-      val firstSubset = content.head
+  private[this] implicit lazy val persistedEdgeFormat = PersistedEdgeFormat
 
-      firstSubset.node1.properties.values.isEmpty mustBe false
-      firstSubset.edge.id.nonEmpty mustBe true
-      firstSubset.node2.properties.values.isEmpty mustBe false
-    }
-  }
+  "The lineage from deployer controller" should {
+    "return the full lineage tree from a context node" in {
+      val deployerid = g.V().has( Constants.TypeKey, "deployer:context" ).asScala.toList.head.id
+      val nodes = g.V( deployerid ).outE( "deployer:launch" ).as( "edge" ).otherV().as( "node" ).repeat( __.bothE( "resource:create", "resource:write", "resource:read" ).dedup().as( "edge" ).otherV().as( "node" ) ).emit().simplePath().select[java.lang.Object]( "edge", "node" )
 
-  "The generic node metadata controller " should {
-    "return a specific nodes metadata" in {
+      val c = ( for ( x <- nodes.asScala.toList ) yield x.asScala.toMap.get( "edge" ).toList ).flatten[Object]
+      val list = for ( i <- c ) yield i.asInstanceOf[util.List[Object]].asScala.toList.length
 
-      val nodeId = g.V().asScala.toList.head.id
-      val nodeMetaData = g.V( nodeId )
-      val result = genericController.retrieveNodeMetaData( nodeId.toString.toLong ).apply( fakerequest )
-      val content = contentAsJson( result ).as[PersistedVertex]
+      val result = lineageController.lineageFromDeployer( deployerid.toString.toLong ).apply( fakerequest )
+      val content = contentAsJson( result ).as[List[JsObject]]
 
-      content.properties.isEmpty mustBe false
-
-      // TODO invent a test that is applicable to all cases
+      ( content.length == list.sum ) mustBe true
 
     }
   }
-  "The generic node metadata controller" should {
-    "return an empty list if the node does not exist" in {
+  "The lineage from deployer controller" should {
+    "return an empty list if the id of the node is not a deployernode" in {
+      val deployerid = g.V().hasNot( "deployer:context" ).asScala.toList.head.id
+      val result = lineageController.lineageFromDeployer( deployerid.toString.toLong ).apply( fakerequest )
+      val content = contentAsJson( result ).as[List[JsObject]]
 
-      val nodeId = 4
-      val result = genericController.retrieveNodeMetaData( nodeId.toString.toLong ).apply( fakerequest )
-      val content = contentAsJson( result )
-
-      ( content.toString() == "null" ) mustBe true
+      content.length mustBe 0
 
     }
   }
 }
-
